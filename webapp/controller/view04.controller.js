a3617ccf59b9680eae53c66f6fe1e2bd17f8f2ef
Dave Ryan Sanz <dave.ryan.r.sanz@accenture.com>
2025-08-19 04:20:08 +0000

    Implement copied directive

--- webapp/controller/view04.controller.js
+++ webapp/controller/view04.controller.js
@@ -1,10 +1,43 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller,MessageToast) => {
     "use strict";
 
     return Controller.extend("my.sapui5.act04.training.act04mvc.controller.view04", {
         onInit() {
-        }
+        },
+
+        onAddItem: function (){
+                this.fnDisplayMsg("Add button pressed");
+            },
+
+        fnDisplayMsg: function (sMsg){
+                MessageToast.show(sMsg);
+            },
+
+        onChangeMOP: function (oEvent) {
+                var sSelectedKey = oEvent.getParameter("selectedItem").getProperty("key");
+                var oMobileLabel = this.getView().byId("idLblPhone");
+                var oMobileInput = this.getView().byId("idInputPhone");
+
+                if (sSelectedKey === "GCASH"){
+                    // show the mobile field
+                    oMobileLabel.setVisible(true);
+                    oMobileInput.setVisible(true);
+                } else {
+                    oMobileLabel.setVisible(false);
+                    oMobileInput.setVisible(false);
+                }
+            },
+
+        onPressCheckout: function (){
+                var oInputFNameValue = this.getView().byId("idInptFName").getValue();
+
+                // Check if first name is blank
+                if (oInputFNameValue === ""){
+                    sap.m.MessageToast.show("Required Field is blank"); 
+                }
+            }
     });
 });
